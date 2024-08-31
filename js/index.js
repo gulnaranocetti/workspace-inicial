cc20c51544868515bad3020b6ce543d8f52fa1fb
Gulnara <gulnaranocetti@gmail.com>
2024-08-31 15:42:25 -0300

    manejo tarjetas index.js

--- js/index.js
+++ js/index.js
@@ -19,6 +19,20 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("user-name").href = "login.html";
       document.getElementById("logout-button").style.display = "none"; 
   }
+
+  // Manejo de la visualización de las tarjetas de categoría
+  document.getElementById("autos").addEventListener("click", function() {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function() {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function() {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
 });
 
 
